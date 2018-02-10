2980c077892e2524bb53e3bba7a4181d886cd4c2
ShengX95 <243725821@qq.com>
2018-02-10 23:53:05 +0800

    web framework init.

--- www/app.py
+++ www/app.py
@@ -1,19 +1,110 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 import logging; logging.basicConfig(level=logging.INFO)
 import asyncio, os, json, time
 from aiohttp import web
 import orm
-#index
-def index(request):
-	return web.Response(body=b'<h1>Awesome<h1/>',content_type='text/html')
+from webcore import add_routes, add_static
+from jinja2 import Environment, FileSystemLoader
 
-@asyncio.coroutine
-def init(evnet_loop):
-	app = web.Application(loop=evnet_loop)
-	app.router.add_route('GET','/',index)
-	srv = yield from evnet_loop.create_server(app.make_handler(), '127.0.0.1', 8080)
+async def logger_factory(app, handler):
+	async def logger(request):
+		logging.info('Request : %s %s' % (request.method, request.path))
+		return (await handler(request))
+	return logger
+
+async def data_factory(app, handler):
+	async def parse_data(request):
+		if request.method == 'POST':
+			if request.content_type.startswith('application/json'):
+				request.__data__ = await request.json()
+				logging.info('request json : %s' % str(request.__data__))
+			elif request.content_type.startswith('application/x-www-form-urlencoded'):
+				request.__data__ = await request.post()
+				logging.info('request form : %s' % str(request.__data__))
+		return (await handler(request))
+	return parse_data
+
+async def response_factory(app, handler):
+	async def response(request):
+		logging.info('response handler...')
+		r = await handler(request)
+		if isinstance(r ,web.StreamResponse):
+			return r
+		if isinstance(r, bytes):
+			resp = web.Response(body=r)
+			resp.content_type='application/octet-stream'
+			return resp
+		if isinstance(r, str):
+			if r.startswith('redirect:'):
+				return web.HTTPFound(r[9:])
+			resp = web.Response(body=r.encode('utf-8'))
+			resp.content_type='text/html;charset=utf-8'
+			return resp
+		if isinstance(r, dict):
+			template = r.get('__template__')
+			if template is None:
+				resp = web.Response(body=json.dumps(r, ensure_ascii=False, default=lambda o: o.__dict__).encode('utf-8'))
+				resp.content_type = 'application/json;charset=utf-8'
+				return resp
+			else:
+				resp = web.Response(body=app['__template__'].get_template(template).render(**r).encode(utf-8))
+				resp.content_type = 'text/html;charset=utf-8'
+		if isinstance(r, int):
+			return web.Response(r)
+		if isinstance(r, tuple):
+			t, m = r
+			if isinstance(t, int) and t >= 100 and t < 600:
+				return web.Response(t, str(m))
+		#default
+		resp = web.Response(body=str(r).encode('utf-8'))
+		resp.content_type = 'text/plain;charset=utf-8'
+		return resp
+
+def init_Jinja2(app, **kw):
+	logging.info('init init_Jinja2')
+	options = dict(
+		autoescape = kw.get('autoescape', True),
+        block_start_string = kw.get('block_start_string', '{%'),
+        block_end_string = kw.get('block_end_string', '%}'),
+        variable_start_string = kw.get('variable_start_string', '{{'),
+        variable_end_string = kw.get('variable_end_string', '}}'),
+        auto_reload = kw.get('auto_reload', True)
+	)
+	path = kw.get('path', None)
+	if path is None:
+		path = os.path.join(os.path.dirname(os.path.abspath(__file__)),'template')
+	logging.info('set jinja2 template path %s' % path)
+	env = Environment(loader=FileSystemLoader(path), **options)
+	filter = kw.get('filter', None)
+	if filter is not None:
+		for name, f in filter.items():
+			env.filters[name] = f
+	app['__template__'] = env
+
+def datetime_filter():
+	pass
+
+async def init(evnet_loop):
+	logging.info('start init datebase...')
+	await orm.Create_Pool(loop=loop,host='192.168.0.221',user='root',password='wjdh84928399',db='test')
+	logging.info('datebase init success!!')
+	logging.info('start init webapplication...')
+	app = web.Application(loop=evnet_loop, middlewares=[
+			logger_factory, response_factory
+		])
+	logging.info('start add routers into webapplication...')
+	init_Jinja2(app, filter=dict(datetime=datetime_filter))
+	add_routes(app, 'handlers')
+	logging.info('end add routers into webapplication...')
+	logging.info('start add static sources into webapplication...')
+	add_static(app)
+	logging.info('end add static sources into webapplication...')
+	logging.info('end init webapplication...')
+	srv = await evnet_loop.create_server(app.make_handler(), '127.0.0.1', 8080)
 	logging.info('server start at http://127.0.0.1:8080..')
 	return srv
+
 loop = asyncio.get_event_loop()
 loop.run_until_complete(init(loop))
-orm.start(loop)
 loop.run_forever()
---
+++ www/webcore.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+def add_routes(app, handlers):
+	pass
+
+def add_static(app):
+	pass
